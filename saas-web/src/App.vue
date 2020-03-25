e2f0cacb5f5bd7de089f1b243e2f0086de7c904e
wxl954136 <wxl_954136@163.com>
2020-03-25 11:21:59 +0800

    Initial commit

---
+++ saas-web/src/App.vue
@@ -0,0 +1,30 @@
+<!--模板结构 -->
+<template>
+  <div id="app">
+    <h1>hello www.youxueyou.cn ！ {{title}}</h1>
+    <users-id></users-id>
+  </div>
+</template>
+
+<!--处理结构  -->
+
+<script>
+import Users from './components/Users'
+export default {
+  name: 'App',
+  data(){
+    return {
+      title:"this my first vue"
+    }
+  },
+  components:{
+    "users-id":Users //注意标签名:users-id
+  }
+}
+</script>
+<!--解决样式 -->
+<style>
+#app {
+
+}
+</style>
---
+++ saas-web/src/components/Users.vue
@@ -0,0 +1,24 @@
+<template>
+   <div id = "Users">
+      <ul>
+          <li v-for="user in Users">
+                {{user}}
+          </li>
+      </ul>
+   </div>
+</template>
+
+<script>
+    export default {
+        name: "Users",
+        data(){
+          return {
+             Users:["王晓陆","姚静","王怿恒"]
+          }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
